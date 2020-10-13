76c6298f613233047ef3dd1c377aad7de4023370
François Parlant <fxpar33@gmail.com>
2020-10-13 19:07:52 +0200

    Add files via upload

---
+++ outils.js
@@ -0,0 +1,5 @@
+document.body.style.border = "5px solid yellow";
+document.getElementById("region-main").style.width = "75%";
+document.getElementById("block-region-side-pre").style.display = "";
+document.getElementById("block-region-side-pre").style.cssText += ';display:block !important; margin:5px !important';
+document.getElementById("block-region-side-pre").style.visibility = "visible";
